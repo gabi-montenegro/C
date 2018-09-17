e944811d40e649c268073c0f607ac753556e6d76
Gabriella Montenegro <gabriellamontenegro2@gmail.com>
2018-09-17 13:12:22 -0300

    ex-2.2

---
+++ ex-2.2/ex2.2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+	int n1, n2, i;
+	printf("Digite n1: ");
+	scanf("%d", &n1);
+	printf("Digite n2: ");
+	scanf("%d", &n2);
+	
+	for (i=n1+1;i<n2;i++){
+		printf("Numeros do intervalo: %i\n", i);
+		
+	}
+	
+	system("pause");
+	return 0;
+}
