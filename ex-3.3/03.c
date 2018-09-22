1f64eaafd95038945fb3e0d2227a412e29c45e65
Gabriella Montenegro <gabriellamontenegro2@gmail.com>
2018-09-22 10:20:48 -0300

    ex-3.3

---
+++ ex-3.3/03.c
@@ -0,0 +1,32 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(){
+	short int temps[49];
+	int i=0, media,soma=0, tempmaior=0;
+
+  //Guardando as temperaturas no vetor
+	while(i<=49){
+		printf("Digite uma temperatura: ");
+		scanf("%d", &temps[i]);
+		soma+=temps[i];
+		i++;
+		
+	}//fim do while
+	media=(soma/i);
+	printf("\nMedia das temperaturas: %d", media);
+
+  //Verificando quantas temperaturas estão acima da média
+	for(i=0;i<=49;i++){
+		if (temps[i]>media){
+				tempmaior+=1;
+      
+    }//fim do if
+  }//fim do for
+  printf("\nQuantidade de temperaturas acima da media: %d\n", tempmaior);
+  system("pause");
+  return 0;
+}//fim do main
+
+//No 03.png, fiz com 4 temperaturas.
