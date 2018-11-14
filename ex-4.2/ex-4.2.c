aaac920a7d71920904a0c79e79c8a56837a71e74
Gabriella Montenegro <43149651+gabi-montenegro@users.noreply.github.com>
2018-11-14 09:31:57 -0200

    Update ex-4.2.c

--- ex-4.2/ex-4.2.c
+++ ex-4.2/ex-4.2.c
@@ -7,7 +7,8 @@ typedef int bool;
 #define false 0
 int eh_primo(int n){
 	bool primo=true;
-	for (int i=2;i<n;i++){
+	int i;
+	for (i=2;i<n;i++){
         if (n%i==0){
             primo=false;
         }
@@ -17,17 +18,26 @@ int eh_primo(int n){
 return primo;
 }
 
-int main(void){
-int n;
-printf("Digite um numero: ");
-scanf("%d", &n);
-for(int i=1;i<=n;i++){
-if (eh_primo(i)){
-    printf("%d: E primo!\n",i);
-}else{
-    printf("%d: Nao e primo!\n",i);
+int todos_os_primos(int n){
+	int i;
+	for(i=1;i<=n;i++){
+		if (eh_primo(i)){
+    			printf("%d: E primo!\n",i);
+		}else{
+    			printf("%d: Nao e primo!\n",i);
 
+}	
+
+
+}//fim do for
 }
-}
+
+int main(void){
+	int n;
+	printf("Digite um numero: ");
+	scanf("%d", &n);
+	todos_os_primos(n);
+
 system("pause");
+return 0;
 }
