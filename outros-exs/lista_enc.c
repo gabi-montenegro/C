72b9e0659ac8df0314d3ad87bf00343f956942e8
gabriella <gabriellamontenegro2@gmail.com>
2022-03-08 18:12:46 -0300

    lista encadeada simples

--- outros-exs/lista_enc.c
+++ outros-exs/lista_enc.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 
-int N;
+int N; //ESTÁTICA
 typedef struct No{
 	char Chave;
 	int Elemento;
@@ -13,6 +13,8 @@ typedef struct ListaLinear{
 } ListaLinear;
 
 void Exibe(struct ListaLinear *L) {
+    //No *p -> PILHA
+    //malloc(sizeof(No)) -> HEAP
     struct No *p = (No*)malloc(sizeof(No));
     p = L->inicio;
     while(p != NULL){
@@ -28,6 +30,8 @@ void Exibe(struct ListaLinear *L) {
 
 //insere no começo da lista
 void Insere(struct ListaLinear *L, int c, int ele) {
+    //int c, int ele, No *novo -> PILHA
+    //malloc(sizeof(No)) -> HEAP
     struct No *novo = (No*)malloc(sizeof(No));
     novo->Chave = c;
     novo->Elemento = ele;
@@ -37,14 +41,16 @@ void Insere(struct ListaLinear *L, int c, int ele) {
 }
 
 int Busca(ListaLinear *L, int c){
+    //int c, No *p -> PILHA
+    //malloc(sizeof(No)) -> HEAP
     struct No *p = (No*)malloc(sizeof(No));
     p = L->inicio;
     while(p != NULL && p->Chave != c){
         p = p->prox;
     }
     if(p != NULL){
-        int elemento = p->Elemento;
-        free(p);
+        int elemento = p->Elemento; //PILHA
+        free(p);//HEAP
         return elemento;
     }else{
         return -1;
@@ -55,19 +61,22 @@ int Busca(ListaLinear *L, int c){
 
 //destroi
 void Destroi(ListaLinear *L){
-    struct No *p = (No*)malloc(sizeof(No));
+    //No *p, No *pprox -> PILHA
+    //malloc(sizeof(No)) -> HEAP
+    struct No *p = (No*)malloc(sizeof(No)); 
     struct No *pprox = (No*)malloc(sizeof(No));
     pprox = L->inicio;
     while(pprox != NULL) {
         pprox = pprox->prox;
         p = pprox;
-        free(p);
+        free(p);//HEAP
     }
 
 }
 
 //k-esimo elem
 int kEsimo(No *p1, int k) {
+    //int k -> PILHA
     if (p1 == NULL){
         return -1;
     }
@@ -79,23 +88,27 @@ int kEsimo(No *p1, int k) {
 }
 
 int main(void){
- struct ListaLinear *L = (ListaLinear*)malloc(sizeof(ListaLinear));
-	//Preenchendo valores previamente determinados em L...
-  Insere(L, 'A', 1);
-  Insere(L, 'B', 2);
-  Insere(L, 'C', 3);
-  Insere(L, 'D', 4);
-  Insere(L, 'E', 5);
-  Exibe(L);
+    //ListaLinear *L -> PILHA
+    //malloc(sizeof(ListaLinear)) -> HEAP
+    struct ListaLinear *L = (ListaLinear*)malloc(sizeof(ListaLinear));
+	
+    Insere(L, 'A', 1);
+    Insere(L, 'B', 2);
+    Insere(L, 'C', 3);
+    Insere(L, 'D', 4);
+    Insere(L, 'E', 5);
+    Exibe(L);
   
 
-  printf("Busca do 'C': %d\n", Busca(L, 'C'));
+    printf("Busca do 'C': %d\n", Busca(L, 'C'));
   
-  struct No *p1 = (No*)malloc(sizeof(No));
-  p1 = L->inicio;
-  printf("K-Esimo 2: %d\n", kEsimo(p1, 2));
+    //No *p1 -> PILHA
+    //malloc(sizeof(No)) -> HEAP
+    struct No *p1 = (No*)malloc(sizeof(No));
+    p1 = L->inicio;
+    printf("K-Esimo 2: %d\n", kEsimo(p1, 2));
 
-  Destroi(L);
+    Destroi(L);
 
 	
 	
