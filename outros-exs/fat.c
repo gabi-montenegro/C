3a91b17b3e05a9e97dfe28aee0101b4f0b692748
Gabriella Montenegro <43149651+gabi-montenegro@users.noreply.github.com>
2022-03-14 16:59:00 -0300

    fatorial recursivo

--- outros-exs/fat.c
+++ outros-exs/fat.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-//na função fat, como a variável n é inteiro (um inteiro ocupa 4 bytes de memória) e temos n (??) chamadas recusivas
+//na função fat, como a variável n é inteiro (um inteiro ocupa 4 bytes de memória) e temos n chamadas recusivas, logo teremos 4n bytes de memória.
 double fat (int n) {
     if (n == 0) {
         return 1;
@@ -8,10 +8,10 @@ double fat (int n) {
     }
 }
 
-double vec[100]; //800 bytes de memória
+double vec[100]; //800 bytes de memória para vec[100], pois um double ocupa 8 bytes de memória, logo 8 * 100 = 800 bytes.
 
 void main (void) {
-    //para cada laço, usa-se 4 bytes de memória
+    //para cada laço, usa-se 4 bytes de memória para a variável i, pois um int equivale a 4 bytes de memória.
     for (int i=0; i<100; i++) {
         vec[i] = fat(i);
     }
