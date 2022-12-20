43ff8c3179b313e8b19b78592c42a2ab25bdf52a
Gabriella Montenegro <gabriellamontenegro2@gmail.com>
2022-12-20 13:25:47 -0300

    bug na escrita sequencial

--- MPI/ex1_v2.c
+++ MPI/ex1_v2.c
@@ -5,7 +5,7 @@
 //https://www.mcs.anl.gov/~itf/dbpp/text/node96.html
 //https://stackoverflow.com/questions/13305814/when-do-i-need-to-use-mpi-barrier
 
-FILE *f;
+// FILE *f;
 
 int main(int argc,char *argv[])
 {
@@ -19,7 +19,7 @@ MPI_Comm_size(MPI_COMM_WORLD, &argc);
 MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
 
-f = fopen(argv[2], "a");
+FILE *f = fopen(argv[2], "a");
 
 // char title[24] = "Send | Receive | Value\n";
 // char string[1000];
@@ -34,9 +34,13 @@ f = fopen(argv[2], "a");
 
 if (rank == 0) { //processo 0
     valor = valor + rank;
-    // printf("Num tasks: %d ---- Rank: %d --- Sou o primeiro\n", argc, rank);
+    
     MPI_Send(&valor, 1, MPI_INT, 1, 1, MPI_COMM_WORLD);
+    printf("\nId: %d ---- Valor: %d\n", rank, valor);
     MPI_Recv(&valor2, 1, MPI_INT, MPI_ANY_SOURCE, 1, MPI_COMM_WORLD, &Stat);
+    
+    // printf("Id: %d ---- Valor: %d\n", Stat.MPI_SOURCE, valor2);
+    
     
     
 } else if(rank == argc-1) { // processo n
@@ -44,6 +48,7 @@ if (rank == 0) { //processo 0
     // printf("Num tasks: %d ---- Rank: %d --- Valor: %d --- Sou o ultimo\n", argc, rank, valor);
     MPI_Recv(&valor2, 1, MPI_INT, argc-2, 1, MPI_COMM_WORLD, &Stat); //recebe do penultimo processo
     MPI_Send(&valor, 1, MPI_INT, 0, 1, MPI_COMM_WORLD); //envia pro Processo 0
+    // printf("Id: %d ---- Valor: %d\n", rank, valor2);
     
 } else { //processo i
     valor = valor + rank;
@@ -51,19 +56,25 @@ if (rank == 0) { //processo 0
     MPI_Recv(&valor2, 1, MPI_INT, rank-1, 1, MPI_COMM_WORLD, &Stat); //recebe do processo i-1
     MPI_Send(&valor, 1, MPI_INT, rank+1, 1, MPI_COMM_WORLD); //envia para o processo i+1
     MPI_Send(&valor, 1, MPI_INT, 0, 1, MPI_COMM_WORLD); //envia pro processo 0
+    // printf("Id: %d ---- Valor: %d\n", rank, valor2);
     
-    
-    
+}
+
+for (int j = 1; j < argc; j++){
+    if (rank == j){
+        printf("Id: %d ---- Valor: %d\n", rank, valor2);
+    }
+    MPI_Barrier(MPI_COMM_WORLD);
 }
 
 // snprintf(string, "  %s |   %s |  %s\n", itoa(rank), itoa(Stat.MPI_SOURCE), itoa(valor));
 // // strcat(itoa())
 // MPI_File_write(f, string, strlen(string)+1, MPI_CHAR, MPI_STATUS_IGNORE);
 
-//MPI_Barrier(MPI_COMM_WORLD);
-printf("Num tasks: %d ---- Rank: %d ---- Recebido por: %d --- Valor: %d\n", argc, rank, Stat.MPI_SOURCE, valor);
 
-fprintf(f, "\n%d | %d | %d\n", rank, Stat.MPI_SOURCE, valor);
+// printf("Num tasks: %d ---- Rank: %d ---- Recebido por: %d --- Valor: %d\n", argc, rank, Stat.MPI_SOURCE, valor);
+
+
 
 
 
