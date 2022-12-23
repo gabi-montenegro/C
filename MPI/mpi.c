8cbc0af1ead81f3970b13e74baa4b898cb086c60
Gabriella Montenegro <gabriellamontenegro2@gmail.com>
2022-12-22 21:12:34 -0300

    adição de média de tempo

--- MPI/mpi.c
+++ MPI/mpi.c
@@ -11,7 +11,11 @@ MPI_Init(&argc,&argv);
 MPI_Comm_size(MPI_COMM_WORLD, &numtasks);
 MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
+double start, end;
+
+
 if (rank == 0) {
+    start = MPI_Wtime();
     dest = 1;
     source = 1;
     outmsg='x';
@@ -29,6 +33,7 @@ else {
     tag, MPI_COMM_WORLD, &Stat);
     rc = MPI_Send(&outmsg, 1, MPI_CHAR, dest,
     tag, MPI_COMM_WORLD);
+    end = MPI_Wtime();
 
 }
 
@@ -36,6 +41,8 @@ MPI_Get_count(&Stat, MPI_CHAR, &count);
 
 printf("Task %d: Received %d char(s) from task %d with tag %d outmsg = %c inmsg = %c \n", rank, count, Stat.MPI_SOURCE, Stat.MPI_TAG, outmsg, inmsg);
 
+MPI_Barrier(MPI_COMM_WORLD);
+if(rank ==1) printf("Tempo: %f", (end-start));
 MPI_Finalize();
 
 return 0;
